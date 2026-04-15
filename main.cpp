7f784d683c23d3f7cf169410c58dd67798ce68d7
codewith-Ranjeet <ranjeetshinde2212@gmail.com>
2026-04-16 01:04:10 +0530

    fixed jump issue

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <optional>
 
+const float gravity = 0.3f;
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Test Window");
@@ -10,7 +12,6 @@ int main()
     float velocityY = 0.0f;
     float groundY = 500.0f;
     float radius = 50.0f;
-    float gravity = 0.3f;
 
     // circle
     sf::CircleShape circle(radius);
@@ -34,37 +35,38 @@ int main()
         ===========================================================================INPUT======================================================================================
         */
         float circleBottom = circle.getPosition().y + radius * 2;
-        // check if object on ground
-        bool onGround = (circleBottom >= groundY);
+        bool onGround = (circleBottom >= groundY); // check if object on ground
 
-
-        /*
-        =================================================================PHYSICS=========================================================================
-        */
-        
-
-        if (onGround)
-        {
-            velocityY = 0;
-            circle.setPosition({circle.getPosition().x, groundY - radius * 2});
-        }
-        velocityY += gravity; // acclerate
-        circle.move({0, velocityY});
-
-        // Movement with keypress
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-            circle.move({2, 0});
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-            circle.move({-2, 0});
         if (((sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space) ||
               sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))) &&
             onGround)
         {
             velocityY = -8.0f; // jump impulse
         }
+
+        /*
+        =================================================================PHYSICS=========================================================================
+        */
+
+        velocityY += gravity; // acclerate
+        circle.move({0, velocityY});
+
+        circleBottom = circle.getPosition().y + radius * 2;
+        //gravity
+        if (circleBottom >= groundY)
+        {
+            circle.setPosition({circle.getPosition().x, groundY - radius * 2});
+            velocityY = 0;
+        }
+
+        // Movement with keypress
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+            circle.move({2, 0});
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+            circle.move({-2, 0});
+
         // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
         //     circle.move({0, 0.2});
-
 
         /*
         ==============================================================================RENDER==========================================================================
