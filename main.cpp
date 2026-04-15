81c85cbfa4332c06da6e76b14abaf5cbaf82ef5c
codewith-Ranjeet <ranjeetshinde2212@gmail.com>
2026-04-16 00:48:08 +0530

    gravity simulated

--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@
 
 int main()
 {
-    // sf::RenderWindow window(sf::VideoMode({800,600}), "My First Window");
     sf::RenderWindow window(sf::VideoMode({800, 600}), "Test Window");
+    window.setFramerateLimit(60);
 
     // gravity
-    float velocityY = 0.01f;
-    int groundY = 500;
-    int radius = 50;
+    float velocityY = 0.0f;
+    float groundY = 500.0f;
+    float radius = 50.0f;
+    float gravity = 0.3f;
 
     // circle
-    sf::CircleShape circle(50); // radius
+    sf::CircleShape circle(radius);
     circle.setFillColor(sf::Color::Red);
     circle.setPosition({200, 200});
 
@@ -29,21 +30,48 @@ int main()
                 window.close();
         }
 
-        window.clear(sf::Color::Black); // bgcolor of window
+        /*
+        ===========================================================================INPUT======================================================================================
+        */
+        float circleBottom = circle.getPosition().y + radius * 2;
+        // check if object on ground
+        bool onGround = (circleBottom >= groundY);
 
-        window.draw(rectangle); // ground
-        window.draw(circle);    // test object (circle)
 
-        int circleBottom = circle.getPosition().y + radius * 2;
+        /*
+        =================================================================PHYSICS=========================================================================
+        */
+        
 
+        if (onGround)
+        {
+            velocityY = 0;
+            circle.setPosition({circle.getPosition().x, groundY - radius * 2});
+        }
+        velocityY += gravity; // acclerate
+        circle.move({0, velocityY});
+
+        // Movement with keypress
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-            circle.move({0.2, 0});
+            circle.move({2, 0});
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-            circle.move({-0.2, 0});
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-            circle.move({0, -0.2});
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-            circle.move({0, 0.2});
+            circle.move({-2, 0});
+        if (((sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space) ||
+              sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))) &&
+            onGround)
+        {
+            velocityY = -8.0f; // jump impulse
+        }
+        // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+        //     circle.move({0, 0.2});
+
+
+        /*
+        ==============================================================================RENDER==========================================================================
+        */
+        window.clear(sf::Color::Black); // bgcolor of window
+        window.draw(rectangle);         // ground
+        window.draw(circle);            // test object (circle)
 
         window.display();
     }
